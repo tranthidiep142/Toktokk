633d1a53112d15fafb838c5dedc496cdb3e16201
diep-tran <diep.tran@navercorp.com>
2023-02-04 14:25:51 +0700

    fix logo of toktok

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
 const images = {
-    logo: require('~/assets/images/logo.svg').default,
+    logo: require('~/assets/images/logoToktok.svg').default,
     noImage: require('~/assets/images/no-image.png'),
 };
 
